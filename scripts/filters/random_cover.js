9ee124498a71fd85f4d2cc06e8d8c87980a7cf84
Rosemoe <2073412493@qq.com>
2023-08-01 15:57:24 +0800

    feat: scan directory files for random covers

--- scripts/filters/random_cover.js
+++ scripts/filters/random_cover.js
@@ -5,6 +5,9 @@
 
 'use strict'
 
+let searchedCoverDirectories = false
+let imageCoverFiles = []
+
 hexo.extend.filter.register('before_post_render', function (data) {
   const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i
   let randomCover
@@ -17,12 +20,41 @@ hexo.extend.filter.register('before_post_render', function (data) {
     if (coverVal && coverVal.indexOf('/') === -1 && imgTestReg.test(coverVal)) data.cover = data.path + coverVal
   }
 
+
   const randomCoverFn = () => {
     const theme = hexo.theme.config
     if (!(theme.cover && theme.cover.default_cover)) return false
-    if (!Array.isArray(theme.cover.default_cover)) return theme.cover.default_cover
-    const num = Math.floor(Math.random() * theme.cover.default_cover.length)
-    return theme.cover.default_cover[num]
+    if (!Array.isArray(theme.cover.default_cover)) {
+      theme.cover.default_cover = [theme.cover.default_cover]
+    }
+    if (!searchedCoverDirectories) {
+      searchedCoverDirectories = true
+      const fs = require('fs')
+      const items = theme.cover.default_cover
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i]
+        if (item.local_path !== undefined && item.web_path !== undefined) {
+          const dirName = item.local_path
+          const webPath = item.web_path
+          if (fs.existsSync(dirName)) {
+            const files = fs.readdirSync(dirName)
+            for (let j = 0; j < files.length; j++) {
+              const file = files[j]
+              if (imgTestReg.test(file)) {
+                imageCoverFiles.push(webPath + '/' + file)
+              }
+            }
+          }
+        } else {
+          imageCoverFiles.push(item)
+        }
+      }
+    }
+    if (imageCoverFiles.length > 0) {
+      const idx = Math.floor(Math.random() * imageCoverFiles.length)
+      return imageCoverFiles[idx]
+    }
+    return false
   }
 
   if (coverVal === false) return data
